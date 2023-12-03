cf0efed4d622355c8ad4dd05c16cc68016fb01d7
Muhammad bilal <meharbilal24525@gmail.com>
2023-12-03 13:07:04 +0500

    Add project files.

---
+++ Anonymous Function or Method/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Anonymous_Function_or_Method
+{
+    public delegate void calculation(int a);
+
+    class Program
+    {
+        public static void addition(int a)
+        {
+            a += 10;
+            Console.WriteLine(a);
+        }
+        public static void addition(calculation del, int a)
+        {
+            a += 10;
+            del.Invoke(a);
+        }
+
+        static void Main(string[] args)
+        {
+            calculation obj = delegate (int a)
+            {
+                a += 10;
+                Console.WriteLine(a);
+            };
+            obj.Invoke(5);
+            calculation obj1 = new calculation(Program.addition);
+            obj1(123);
+            Program.addition(delegate (int a)
+            {
+                a += 10;
+                Console.WriteLine(a);
+            }, 12);
+            Console.ReadLine();
+        }
+    }
+}
